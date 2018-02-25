21362b94da70b5348cc9e829079882caf21f46ce
nadezhda-kotova <36230086+nadezhda-kotova@users.noreply.github.com>
2018-02-25 23:12:59 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let breakstring = equation.split (' ');
+  let a = Number(breakstring[0]);
+  let b = Number(breakstring[3]+breakstring[4]);
+  let c = Number(breakstring[7]+breakstring[8]);
+  let discriminant = Math.pow(b, 2) - 4 * a * c;
+  let x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+  let x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+  return [Math.round(Math.min(x1, x2)), Math.round(Math.max(x1, x2))];
 }
